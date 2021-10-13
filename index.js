513c9424e81605ef248c5af91b2bff217dd25d51
Sofia Alvarez <sofia.alvarez92@hotmail.com>
2021-10-13 13:21:59 -0300

    primer arreglo js

---
+++ index.js
@@ -0,0 +1,11 @@
+let Ubicacion = prompt("Ingresa tu pais").toLowerCase; {
+
+    if (Ubicacion = "Argentina") {
+        alert("Bienvenido a Alsodeco")
+        console.log("Ubicacion aceptada");
+    } else {
+        alert("Solo tenemos ventas en Argentina")
+        console.log("Ubicacion rechazada");
+        break;
+    }
+}
