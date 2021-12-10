aa81d8f481366911b9a8e0eb6fc2313337b25fc6
Sofia Alvarez <sofia.alvarez92@hotmail.com>
2021-12-10 15:07:24 -0300

    ajustes1

--- js/main.js
+++ js/main.js
@@ -43,56 +43,7 @@ function comprobar(){
 }
 
 
-/// PRODUCTOS ///
 
-let botonCompra = document.querySelectorAll(".botonCompra");
-let carrito = [];
-console.log( botonCompra );
-
-
-for (let boton of botonCompra){
-    boton.addEventListener("click", agregarCarrito);
-
-}
-
-function agregarCarrito(e){
-    
-    let hijo = e.target;
-    let padre = hijo.parentNode.parentNode;
-    let nombreProducto = padre.querySelector("h2").textContent;
-    let img = padre.querySelector("img").src;
-    let precio = padre.querySelector("p").textContent;
-
-
-    const producto = {
-
-        nombre: nombreProducto,
-        img: img,
-        precio: precio,
-        cantidad: 1
-
-    }
-
-
-    carrito.push(producto);
-    mostrarCarrito(producto);
-
-}
-
-function mostrarCarrito(producto){
-    let fila = document.createElement("tr");
-    fila.innerHTML = ` <td>${producto.nombre}</td>
-                        <td>${producto.cantidad}</td>
-                        <td>${producto.precio}</td>
-                        <td><button class="btn btn-danger">Eliminar</button></td>
-                    `
-    let tbody = document.getElementById("tbody");
-    
-    tbody.appendChild(fila)
-}
-
-
-console.log (carrito);
 
 ///MAPA///
 
